ec79605e01915e75557d82f2c31d9ac691d653bd
Archana <archu30689@gmail.com>
2022-03-31 11:46:07 +0200

    madechange in testdata file

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -9,10 +9,10 @@ class loginpage{
         let Actuallist=[]
         const listoftabs=await $$('li.nav-menu-list-container>ul[class=nav-menu-list]>li')
         for(let i =0;i<listoftabs.length;i++){
-           // console.log(await (await(listoftabs[i]).getText()))
+           console.log(await (await(listoftabs[i]).getText()))
             actuallist = actuallist+","+(await(listoftabs[i]).getText())
              Actuallist = await(await (actuallist).slice(1,await (actuallist).length))
-             //actuallist = (await(listoftabs[i]).getText())+ ","+actuallist
+             actuallist = (await(listoftabs[i]).getText())+ ","+actuallist
          
              }
               return Actuallist
